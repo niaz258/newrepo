882bbc5de778e11663eb3c26af53b53715f16fd8
niaz258 <niaz_258@yahoo.com>
2025-07-27 17:43:44 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import joblib
+model = joblib.load('iris_model')
+
+app = FastAPI()
+
+@app.get("/")
+def predict_iris(sl:float,sw:float,pl:float,pw:float):
+    result = model.predict([[sl,sw,pl,pw]])
+    return {'Predicted class is':int(result[0])}
+    
+    
